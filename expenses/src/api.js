d667f4a510fe0712674d5275663e07cc715fa3df
binuri2018 <imbinuri@gmail.com>
2025-09-09 00:28:15 +0530

    deploy test fixes frontend

--- expenses/src/api.js
+++ expenses/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:5000/api";
+const BASE_URL = "https://expenses-track-flask-1.onrender.com";
 
 // -------------------
 // Auth routes
