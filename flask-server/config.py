b4d862e4211e16a0065057820e33d37f0f735c13
binuri2018 <imbinuri@gmail.com>
2025-09-09 01:51:25 +0530

    deploy test fixes cores

--- flask-server/config.py
+++ flask-server/config.py
@@ -2,6 +2,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://pbinu5083:aEnpsmXjdomEuR6o@cluster0.ooictsa.mongodb.net/flask_ex")
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    "mongodb+srv://pbinu5083:aEnpsmXjdomEuR6o@cluster0.ooictsa.mongodb.net/flask_ex"
+)
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "AB2018")
-CORS_ORIGINS = os.getenv("CORS_ORIGINS", "http://localhost:3000")
+
+# Split multiple origins by comma and strip spaces
+CORS_ORIGINS = [origin.strip() for origin in os.getenv(
+    "CORS_ORIGINS",
+    "http://localhost:3000,https://flsexpense.netlify.app,https://expenses-track-flask-2.onrender.com"
+).split(",")]
